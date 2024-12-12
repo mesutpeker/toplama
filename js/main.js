688197522bb4adacc7720096d5000237c4331241
mesutpeker <44334462+mesutpeker@users.noreply.github.com>
2024-12-12 15:59:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+// Quiz oyununu başlat
+window.quizGame = new QuizGame();
 
+// İlk soruyu göster
+window.quizGame.displayQuestion();
+
+// 3D efektleri ekle
+AnimationUtils.addFloatingEffect(document.querySelector('.quiz-box'));
